511d8f87e7692d6660b24f1dbd7a3aef93c2b963
akanandhu <akananthu11@gmail.com>
2024-09-01 15:52:51 +0530

    initial files

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+// import { BrowserRouter } from "react-router-dom";
+import Approuter from "./approuter";
+
+// import { App } from "./app";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./assets/css/bootstrap.min.css";
+//CSS & Bootstrap
+import "./assets/css/style.css";
+//  import "./assets/js/bootstrap.bundle.min.js";
+import "./assets/css/select2.min.css";
+//Font Awesome
+// import "./assets/plugins/fontawesome/css/fontawesome.min.css";
+// import "./assets/plugins/fontawesome/css/all.min.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <Approuter/>
+);
