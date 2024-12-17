550817cedeae32d322c6259b550cbfaeff6edb4b
akanandhu <akananthu11@gmail.com>
2024-12-17 21:49:38 +0530

    chore: ui changes

--- src/components/Dashboard/Settlement/ReviewFasttag.jsx
+++ src/components/Dashboard/Settlement/ReviewFasttag.jsx
@@ -1,21 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import Select from "react-select";
 import FasttagToggle from "../../FasttagRevenue/FasttagToggle";
 import { pencil_icon } from "../../imagepath";
 import { Link, useParams } from "react-router-dom";
 
 function ReviewFasttag() {
-  const { id } = useParams(); 
-  const [selectedOption, setSelectedOption] = useState({
-    label: "10",
-    value: "10",
-  });
-  const [tagsCount] = useState([
-    { label: "10", value: "10" },
-    { label: "15", value: "15" },
-    { label: "20", value: "20" },
-    { label: "25", value: "25" },
-  ]);
+  const { id } = useParams();
   return (
     <div className="card-box">
       <div className="row">
@@ -26,17 +16,6 @@ function ReviewFasttag() {
           <Link to={`/manage-hospitals/manage-request/${id}/edit`}>
             <img src={pencil_icon} alt="edit" width={18} height={18} />
           </Link>
-        </div>
-      </div>
-
-      <div className="mt-3">
-        <h5>Select Fast Tag Count Per Day</h5>
-        <div className="w-sm-50 w-25">
-          <Select
-            defaultValue={selectedOption}
-            onChange={setSelectedOption}
-            options={tagsCount}
-          />
         </div>
       </div>
 
